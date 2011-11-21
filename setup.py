b1fb1c6e5f79585343fbb2213e8d22acee29b91a
Lance Hepler <nlhepler@gmail.com>
2011-11-21 14:59:12 -0800

    fix setup.py to use the new module version

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
+import sys
+
+from os.path import abspath, join, split
 from setuptools import setup
 
+sys.path.insert(0, join(split(abspath(__file__))[0], 'lib'))
+from hypy import __version__ as _hypy_version
+
 setup(name='hypy',
-      version='0.1.0',
+      version=_hypy_version,
       description='An intuitive HyPhy interface for Python',
       author='N Lance Hepler',
       author_email='nlhepler@gmail.com',
       url='http://github.com/nlhepler/hypy',
       license='GNU GPL version 3',
       packages=['hypy'],
-      package_dir={'hypy': 'src/hypy'}
+      package_dir={'hypy': 'lib/hypy'}
      )
