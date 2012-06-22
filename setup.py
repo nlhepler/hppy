8b3cfde6b0796d2e9c2890e2e3666b5c7a8f7502
Lance Hepler <nlhepler@gmail.com>
2012-06-22 14:43:19 -0700

    rename hypy to hppy so we can upload to pypi

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ from os.path import abspath, join, split
 from setuptools import setup
 
 sys.path.insert(0, join(split(abspath(__file__))[0], 'lib'))
-from hypy import __version__ as _hypy_version
+from hppy import __version__ as _hppy_version
 
-setup(name='hypy',
-      version=_hypy_version,
+setup(name='hppy',
+      version=_hppy_version,
       description='An intuitive HyPhy interface for Python',
       author='N Lance Hepler',
       author_email='nlhepler@gmail.com',
-      url='http://github.com/nlhepler/hypy',
+      url='http://github.com/nlhepler/hppy',
       license='GNU GPL version 3',
-      packages=['hypy'],
-      package_dir={'hypy': 'lib/hypy'}
+      packages=['hppy'],
+      package_dir={'hppy': 'lib/hppy'}
      )
